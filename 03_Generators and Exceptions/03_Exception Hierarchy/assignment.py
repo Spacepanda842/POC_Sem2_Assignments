ae8a3ba5b50112dbe243a78e05e8063762c0b704
Kenyan Wilson <wilsoken008@warren.k12.in.us>
2024-01-25 09:19:03 -0500

    Generators and Exceptions.

---
+++ 03_Generators and Exceptions/03_Exception Hierarchy/assignment.py
@@ -0,0 +1,9 @@
+number_1 = int(input("Enter a number:"))
+number_2 = int(input("Enter another number:"))
+try:
+    print(number_1, "Divided by", number_2, "=", int(number_1/number_2))
+except ZeroDivisionError:
+    print("Sorry, you divided by zero, that's literally impossible goofy. For example, if you have  and divide it by 10, YOU CAN'T DIVIDE ABSOLUTELY NOTHING, which means the answer is.......Hold up...it's zero💀")
+
+
+
