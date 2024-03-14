d2892cb8b7ed28c7edc7e3b64fe35ec71d789edc
Kenyan Wilson <wilsoken008@warren.k12.in.us>
2024-03-14 09:49:09 -0400

    square mehod

--- 08_OOP/05_Methods/assignment.py
+++ 08_OOP/05_Methods/assignment.py
@@ -1,31 +1,51 @@
 class Rectangle:
+    
+    __counter = 0
 
+    def get_count():
+        return Rectangle.__counter
+    
     def __init__(self, base: float, height: float) -> None:
-        if (base < 0):
+        if(base < 0):
             self.__base = 0
-        else:
+        else:       
             self.__base = base
-
-        if (height < 0):
+            
+        if(height < 0):
             self.__height = 0
         else:
             self.__height = height
 
+        Rectangle.__counter += 1
+    
     def get_height(self) -> float:
         return self.__height
-
-    # YOUDO the get_base method
-
+    
+    def get_base(self)-> float:
+        return 1 * self.__base + 1 * self.__height
+    
     def get_perimeter(self) -> float:
         return 2 * self.__base + 2 * self.__height
-
-    # Youdo get_area method
     
-    def __str__(self) -> str:
-        #Rectangle with base:3, height:4
-        return "Rectangle with base:" + str(self.__base) + ", height:" + str(self.__height)
+    def get_area(self) -> float:
+        return 3 * self.__base + 3 * self.__height
     
+    def string(self) -> float:
+        return 4 *self.__base + 3 * self.__height
 
+ 
+rectangle_1 = Rectangle(5,10)
+print("The base of the rectangle is", rectangle_1.get_base())
+print("The height of the rectangle is", rectangle_1.get_height())
+print("The perimeter of the rectangle is", rectangle_1.get_perimeter())
+print("The area of the rectangle is", rectangle_1.get_area())
+print(rectangle_1.string)
 
-# YOUDO>  create two rectangles.  print their base, height, perimeter, and area
-# using only the methods not the fields/property/attributes
+rectangle_2 = Rectangle(1, 6)
+print("The base of the rectangle is", rectangle_2.get_base())
+print("The height of the rectangle is", rectangle_2.get_height())
+print("The perimeter of the rectangle is", rectangle_2.get_perimeter())
+print("The area of the rectangle is", rectangle_2.get_area())
+print("The counting, I guess? Of Rectangles is", Rectangle.get_count())
+print(rectangle_2.string)
+ 
