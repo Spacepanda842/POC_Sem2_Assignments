ae036dd3a279614b19e0237ff379014cb1759240
Kenyan Wilson <wilsoken008@warren.k12.in.us>
2024-03-15 09:38:00 -0400

    str method

--- 08_OOP/05_Methods/assignment.py
+++ 08_OOP/05_Methods/assignment.py
@@ -1,10 +1,5 @@
 class Rectangle:
     
-    __counter = 0
-
-    def get_count():
-        return Rectangle.__counter
-    
     def __init__(self, base: float, height: float) -> None:
         if(base < 0):
             self.__base = 0
@@ -15,8 +10,6 @@ class Rectangle:
             self.__height = 0
         else:
             self.__height = height
-
-        Rectangle.__counter += 1
     
     def get_height(self) -> float:
         return self.__height
@@ -30,8 +23,8 @@ class Rectangle:
     def get_area(self) -> float:
         return 3 * self.__base + 3 * self.__height
     
-    def string(self) -> float:
-        return 4 *self.__base + 3 * self.__height
+    def __str__(self) -> str:
+        return "Rectangle with base:" + str (self.__base) + ", height:" + str (self.__height)
 
  
 rectangle_1 = Rectangle(5,10)
@@ -39,13 +32,12 @@ print("The base of the rectangle is", rectangle_1.get_base())
 print("The height of the rectangle is", rectangle_1.get_height())
 print("The perimeter of the rectangle is", rectangle_1.get_perimeter())
 print("The area of the rectangle is", rectangle_1.get_area())
-print(rectangle_1.string)
+print(rectangle_1)
 
 rectangle_2 = Rectangle(1, 6)
 print("The base of the rectangle is", rectangle_2.get_base())
 print("The height of the rectangle is", rectangle_2.get_height())
 print("The perimeter of the rectangle is", rectangle_2.get_perimeter())
 print("The area of the rectangle is", rectangle_2.get_area())
-print("The counting, I guess? Of Rectangles is", Rectangle.get_count())
-print(rectangle_2.string)
+print(rectangle_2)
  
